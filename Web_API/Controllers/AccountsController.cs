f2e5116e4829bc51af418fc0057c3846e2ff898b
Sebastian Chłoń <Sebastiaan.chlon@gmail.com>
2022-09-25 17:38:07 +0200

    Add project files.

---
+++ Web_API/Controllers/AccountsController.cs
@@ -0,0 +1,157 @@
+﻿
+using Web_API.Data;
+using Web_API.Model;
+
+namespace Web_API.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly MySomeNameAplicationContext _context;
+
+        public AccountsController(MySomeNameAplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        [HttpGet("GetAccounts")]
+        public async Task<ActionResult<List<Accounts>>> GetAccounts()
+        {
+              return Ok(await _context.Accounts.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        [HttpGet("GetSomeAccount")]
+        public async Task<ActionResult<List<Accounts>>> GetSomeAccount(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var accounts = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (accounts == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(accounts);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nick,Login,Password,Name,Surname,Active_Account,Date_Creation,Date_Deactivation")] Accounts accounts)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(accounts);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(accounts);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var accounts = await _context.Accounts.FindAsync(id);
+            if (accounts == null)
+            {
+                return NotFound();
+            }
+            return View(accounts);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nick,Login,Password,Name,Surname,Active_Account,Date_Creation,Date_Deactivation")] Accounts accounts)
+        {
+            if (id != accounts.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(accounts);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountsExists(accounts.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(accounts);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var accounts = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (accounts == null)
+            {
+                return NotFound();
+            }
+
+            return View(accounts);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Accounts == null)
+            {
+                return Problem("Entity set 'MySomeNameAplicationContext.Accounts'  is null.");
+            }
+            var accounts = await _context.Accounts.FindAsync(id);
+            if (accounts != null)
+            {
+                _context.Accounts.Remove(accounts);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountsExists(int id)
+        {
+          return _context.Accounts.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Web_API/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Mvc.Rendering;
